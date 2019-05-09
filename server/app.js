d1f41ef020338a8dc63dee26178721786242d57e
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-05-09 19:55:03 +0200

    redeploy

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -61,7 +60,6 @@ require('./passport')(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
